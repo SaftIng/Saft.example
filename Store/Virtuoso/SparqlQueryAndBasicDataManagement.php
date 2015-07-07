5b24d74b05e67c0cbc3ca1d0f1e838a2f83e4ac5
Konrad Abicht <k.abicht@googlemail.com>
2015-07-07 14:36:41 +0200

    correct and extend Store/Virtuoso/SparqlQueryAndBasicDataManagement.php

--- Store/Virtuoso/SparqlQueryAndBasicDataManagement.php
+++ Store/Virtuoso/SparqlQueryAndBasicDataManagement.php
@@ -50,9 +50,25 @@ $virtuoso->addStatements(
 );
 
 // ask graph forall its triples and print them out
-$statementIterator = $virtuoso->query('SELECT ?s FROM <http://saft/test> WHERE {?s ?p ?o}');
-foreach ($statementIterator as $key => $statement) {
-    echo (string) $statement;
+$setIterator = $virtuoso->query('SELECT ?s FROM <http://saft/test> WHERE {?s ?p ?o}');
+foreach ($setIterator as $key => $entry) {
+
+    echo PHP_EOL . $entry['s']->getUri();
+
+    // Saft will check the value of each binding and uses the appropriate Node type.
+    // we can use getUri here, because we know that it can only be an URI, but in case
+    // you dont know, you can use the following isX methods to determine the type of the
+    // binding.
+
+    if ($entry['s']->isNamed()) {
+        echo PHP_EOL .'binding s is a named node.'; // e.g. NamedNodeImpl
+    } elseif ($entry['s']->isLiteral()) {
+        echo PHP_EOL .'binding s is a literal.'; // e.g. LiteralImpl
+    } elseif ($entry['s']->isBlank()) {
+        echo PHP_EOL .'binding s is a blank node.'; // e.g. BlankNodeImpl
+    } elseif ($entry['s']->isPattern()) {
+        echo PHP_EOL .'binding s is a pattern/placeholder/variable.'; // e.g. AnyPatternImpl
+    }
 }
 
 // drop test graph
